dc60b4be5fcdb776913436126976398f9427335c
Marin-GHUB <74658382+Marin-GHUB@users.noreply.github.com>
2021-08-24 13:18:00 +0200

    Add files via upload

--- Apps/backend.py
+++ Apps/backend.py
@@ -1,9 +1,11 @@
-from dash.dependencies import Input, Output
-import pandas as pd
 import joblib
 import numpy as np
+import pandas as pd
+from dash.dependencies import Input, Output
 from flask import Flask, escape, request
-from frontend import create_dash_application, result_text
+
+from frontend import (client_id, create_dash_application, path_home,
+                      path_infos, result_color, result_text, regex)
 
 # 100001 not favorable and 100106 favorable
 
@@ -33,27 +35,66 @@ def predict_score(client_id):
     
 # Checking the ID and returning the possibility of loan
 @frontend_app.callback(
-    Output(component_id='result_text', component_property='children'),
-    Input(component_id='client_id', component_property='value')
+    Output('result_text', 'children'),
+    Output('result_text', 'color'),
+    Output('infos_path', 'href'),
+    Output('home_path', 'href'),
+    Input('client_input', 'value')
 )
-def say_ID(input_value):
+def say_loan(input_value):
     if input_value in ['', None]:
         result_text = ''
+        result_color = 'light'
+        path_infos = f"/infos"
+        path_home = f"/"
     else:
         try :
-            int(input_value)
-            if int(input_value) in list(client_df['SK_ID_CURR']):
-                result = predict_score(int(input_value))
+            ID_temp = int(input_value)
+            path_infos = f"/infos?{ID_temp}"
+            path_home = f"/?{ID_temp}"
+            if ID_temp in list(client_df['SK_ID_CURR']):
+                result = predict_score(ID_temp)
                 if result == 0:
                     result_text = f'The client with ID {input_value} is not favorable for a loan.'
+                    result_color = 'danger'
                 else:
                     result_text = f'The client with ID {input_value} is favorable for a loan.'
+                    result_color = 'success'
             else:
                 result_text = 'The ID you entered is not in the database.'
+                result_color = 'info'
         except:
             result_text = 'The ID must be composed ONLY of NUMBERS.'
-    #result_text = '{}'.format(i)
-    return result_text
+            result_color = 'warning'
+            path_infos = f"/infos"
+            path_home = f"/"
+    return result_text, result_color, path_infos, path_home
+
+# Getting the ID in the infos page
+@frontend_app.callback(
+    Output('shown_client_id', 'children'),
+    Input('client_id', 'data')
+)
+def retrieve_client_id(input_data):
+    if input_data == None :
+        text_shown = ''
+    else :
+        text_shown = f'The ID of the client you entered was : {input_data}.'
+
+    return text_shown#, input_data
+
+# Keeping the ID when returning to the home page
+@frontend_app.callback(
+    Output('client_input', 'value'),
+    Input('home_path', 'href')
+)
+def keeping_client_id(input_value):
+    temp_list = regex.split(input_value)
+    if len(temp_list) > 1 :
+        ID_temp = temp_list[-1]
+    else : 
+        ID_temp = None
+    return ID_temp
 
 #################################################################
 # Running the app
--- Apps/frontend.py
+++ Apps/frontend.py
@@ -1,3 +1,6 @@
+import re
+from re import search
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -6,35 +9,132 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
+from dash.development.base_component import Component
+from numpy.core.fromnumeric import searchsorted
 
 #################################################################
 # Initialisation of variables
-client_result = ''
+path_infos = f"/infos"
+path_home = f"/"
 result_text = ''
+result_color = 'success'
+client_id = dcc.Store(id='client_id', storage_type='local')
+regex = re.compile('\?')
 
 #################################################################
-# Describing the Layout
+### Describing the entire Layout
+# Styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# Padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+# Describing the sidebar
+sidebar = html.Div(
+    [
+        html.H1("Navigation", className="display-5", style={'textAlign':'center'}),
+        html.Hr(),
+        html.P("Estimation d'Allocation de Prêt", className="lead", style={'textAlign':'center'}),
+        dbc.Nav(
+            [
+                dbc.NavLink("Accueil", href=path_home, active="exact", id='home_path'),
+                dbc.NavLink("Informations complémentaires", href=path_infos, active="exact", id='infos_path'),
+                ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+# Describing the page content
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+# Describing the meta layout
 layout = html.Div([
-    dbc.Row(dbc.Col(html.H1('This is a test'))),
-    dbc.Row([
-        dbc.Col(html.P('Enter the ID you want :')),
-        dbc.Col(dbc.Input(id='client_id', placeholder='Enter ID')),
-        dbc.Col(dbc.Label(result_text, id = 'result_text')),
-        ]) 
+    dcc.Location(id="url"),
+    sidebar,
+    content
 ])
 
+# Describing the home page
+accueil = [
+    dbc.Row(dbc.Col(html.H1('Prêt à Dépenser', style={'textAlign':'left'})), justify='center'),
+    dbc.Row(html.Hr()),
+    dbc.Row(html.Hr()),
+    dbc.Row([
+        dbc.Col(html.P("Merci de bien vouloir rentrer l'identifiant du client:"), width={'size':3}),
+        dbc.Col(dbc.Input(id='client_input', placeholder='Enter ID', value=None), width={'size':2}),
+        ], no_gutters=True),
+    dbc.Row(html.Hr()),
+    dbc.Row(dbc.Col(dbc.Label(result_text, id = 'result_text', color=result_color)))
+]
+
+# Describing the infos page
+infos = [
+    dbc.Row(dbc.Col(html.H1('Test Info Page'))),
+    dbc.Row([
+        dbc.Col(dbc.Label(client_id, id='shown_client_id')),
+    ])
+]
+
 #################################################################
-# Calling the app in Flask
+### Calling the app in Flask
 def create_dash_application(flask_app):
+    # Creating the app
     dash_app = dash.Dash(
         server=flask_app,
         name='front_end',
         url_base_pathname='/',
         external_stylesheets=[dbc.themes.SLATE]
     )
-
+    
+    # Display Layout
     dash_app.layout = layout
 
+    # Processing page content
+    @dash_app.callback(
+        Output("page-content", "children"),
+        [Input("url", "pathname")]
+    )
+    def render_page_content(pathname):
+        if pathname == "/":
+            return accueil
+        elif pathname == "/infos":
+            return infos
+        # Creating an error 404 page
+        else:
+            return dbc.Jumbotron([
+                    html.H1("404: Not found", className="text-danger"),
+                    html.Hr(),
+                    html.P(f"The pathname {pathname} was not recognised...")
+                    ])
+
+    # Storing the ID in the data storage
+    @dash_app.callback(
+        Output('client_id', 'data'),
+        [Input('infos_path', 'href')]
+    )
+    def get_client_id(input_value):
+        temp_list = regex.split(input_value)
+        if len(temp_list) > 1 :
+            ID_temp = temp_list[-1]
+        else : 
+            ID_temp = None
+        return ID_temp
+        
     return dash_app
     
 #################################################################
